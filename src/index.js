715e675d43009032a92469627d9dbbf2873df3d1
darwins-learning <darwinslearning@gmail.com>
2022-07-22 07:55:34 +0530

    VBefore Redux

--- src/index.js
+++ src/index.js
@@ -1,20 +1,46 @@
-import React, { StrictMode } from 'react';
+import React, { StrictMode, useState } from 'react';
 import { createRoot } from 'react-dom/client';
+import { createStore } from 'redux';
+//import reducer from './reducers/reducer'
+
+//const store = createStore(reducer)
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 const formStyle = {
-  color: "red"
+  color: "red",
+  textAlign: "center"
 }
+
+
 
 function MyForm() {
 
+  const [ value, setValue ] = useState("oh")
+
+  const handleChange =  (e) => {
+    let value = e.target.value
+    setValue(value)
+    alert({value}.toString())
+  }
+  
+  const handleSubmit = (e) => {
+      e.preventDefault()
+      alert("hello")
+  }
+
   return(
+    <>
+    <div id="heading">
+      <h3 style={{ textAlign: "center" }}>Get Who is Result</h3>
+    </div>
     <form style={formStyle}>
       <label for="urlText">Enter Url: </label>
-      <input type="text" id="urlText" className="urlText" name="urlText" style={{ color: "inherit"}}/> 
+      <input type="text" id="urlText" className="urlText" name="urlText" style={{ color: "inherit"}} onChange={handleChange} />
+      <input type="submit" id="submit" onClick={handleSubmit} /> 
     </form>
+    </>
   )
 }
 

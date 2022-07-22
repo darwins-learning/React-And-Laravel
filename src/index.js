a0d63ea96d4ff9f9a235f1eb70f3dfed7d4e0fdc
darwins-learning <darwinslearning@gmail.com>
2022-07-22 06:27:50 +0530

    Added Form Layout

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App';
-
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
+const formStyle = {
+  color: "red"
+}
+
+function MyForm() {
+
+  return(
+    <form style={formStyle}>
+      <label for="urlText">Enter Url: </label>
+      <input type="text" id="urlText" className="urlText" name="urlText" style={{ color: "inherit"}}/> 
+    </form>
+  )
+}
+
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <MyForm />
 );
